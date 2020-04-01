1772c51d9cbca20233d3bc2f9dfe7a78c4496d22
Alessandro Levante <62677560+PinoMartirio@users.noreply.github.com>
2020-04-01 19:40:48 +0200

    Updated Italian translation
    Added one string that wasn't translated.

--- lua/lang/italian/shinigami.lua
+++ lua/lang/italian/shinigami.lua
@@ -7,4 +7,4 @@ L["search_role_" .. SHINIGAMI.abbr] = "Questa persona era uno Shinigami!"
 L["target_" .. SHINIGAMI.name] = "Shinigami"
 L["ttt2_desc_" .. SHINIGAMI.name] = [[Lo Shinigami è un Innocente (che collabora con gli altri innocenti) ed il suo obiettivo è quello di uccidere tutti i ruoli malvagi ^^ Lo Shinigami può vedere il nome dei suoi nemici.]]
 
-L["ttt2_shinigami_chat_jammed"] = "The Chat is jammed! You can't use the chat as a respawned Shinigami."
+L["ttt2_shinigami_chat_jammed"] = "La Chat è bloccata! Non puoi usare la chat da Shinigami respawnato."

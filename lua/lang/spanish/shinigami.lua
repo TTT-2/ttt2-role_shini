944a97ea0bc19ca112ba008627dd26f3f9835fe9
Tekiad <tekiadp@gmail.cm>
2020-07-31 16:37:00 -0300

    Create shinigami.lua (#12)
    * Create shinigami.lua
    
    shinigami spanish translation
    
    * "spanish" to "Español"

---
+++ lua/lang/spanish/shinigami.lua
@@ -0,0 +1,12 @@
+L = LANG.GetLanguageTableReference("Español")
+
+L[SHINIGAMI.name] = "Shinigami"
+L["info_popup_" .. SHINIGAMI.name] = [[¡Te has convertido en un Shinigami! Intenta llevarte a la mayor cantidad de traidores que puedas.]]
+L["body_found_" .. SHINIGAMI.abbr] = "¡Era un Shinigami"
+L["search_role_" .. SHINIGAMI.abbr] = "Esta persona era un Shinigami."
+L["target_" .. SHINIGAMI.name] = "Shinigami"
+L["ttt2_desc_" .. SHINIGAMI.name] = [[El Shinigami es un inocente que coopera con sus compañeros ya que no sabe que lo es hasta su muerte. Al morir, este resucitará 
+y obtendrá velocidad y daño pero su vida decae con el tiempo, considéralo una última chance de colaborar. Además, podrá ver a los traidores vivos pero no podrá 
+comunicarse con su equipo.]]
+
+L["ttt2_shinigami_chat_jammed"] = "¡El chat está prohibido! No puedes usar el chat siendo Shinigami."

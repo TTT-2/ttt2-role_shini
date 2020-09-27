1a40831f37bbd2551dc3291b2fc42507369faba5
Blueberryy <igory.ygr200@gmail.com>
2020-09-27 13:01:40 +0500

    Create shinigami.lua

---
+++ lua/terrortown/lang/russian/shinigami.lua
@@ -0,0 +1,10 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+L[SHINIGAMI.name] = "Шинигами"
+L["info_popup_" .. SHINIGAMI.name] = [[Вы Шинигами! Попробуйте убить злых террористов!]]
+L["body_found_" .. SHINIGAMI.abbr] = "Он был Шинигами."
+L["search_role_" .. SHINIGAMI.abbr] = "Этот человек был Шинигами!"
+L["target_" .. SHINIGAMI.name] = "Шинигами"
+L["ttt2_desc_" .. SHINIGAMI.name] = [[Шинигами - Невиновный (который работает вместе с другими невиновными), и его цель - убить всех злых ролей ^^ Шинигами может видеть имена своих врагов.]]
+
+L["ttt2_shinigami_chat_jammed"] = "Чат заклинило! Вы не можете использовать чат как возродившийся Шинигами."

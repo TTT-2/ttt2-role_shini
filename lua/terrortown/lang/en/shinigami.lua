4d80a6a5a5778b0ca5cce7e1f3bf46287b210d42
SrB54 <srb5467@gmail.com>
2021-11-04 18:25:05 +0000

    Small language fixes

--- lua/terrortown/lang/en/shinigami.lua
+++ lua/terrortown/lang/en/shinigami.lua
@@ -5,7 +5,7 @@ L["info_popup_" .. SHINIGAMI.name] = [[You are a Shinigami! Try to kill the evil
 L["body_found_" .. SHINIGAMI.abbr] = "They were a Shinigami."
 L["search_role_" .. SHINIGAMI.abbr] = "This person was a Shinigami!"
 L["target_" .. SHINIGAMI.name] = "Shinigami"
-L["ttt2_desc_" .. SHINIGAMI.name] = [[The Shinigami is an Innocent (who works together with the other innocents) and the goal is to kill all evil roles ^^ The Shinigami is able to see the names of his enemies.]]
+L["ttt2_desc_" .. SHINIGAMI.name] = [[The Shinigami is an Innocent (who works together with the other innocents) with the goal of killing all evil roles ^^ The Shinigami is able to see the names of their enemies.]]
 
 L["ttt2_shinigami_chat_jammed"] = "The Chat is jammed! You can't use the chat as a respawned Shinigami."
 L["ttt2_shinigami_revive"] = "You are revived as a Shinigami. Take your knife, be quiet and kill those pesky Traitors!"
